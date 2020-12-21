576e89f701ec402d56068d4eef7cad340f0cc276
jbande <bandhell@gmail.com>
2020-12-21 11:18:52 -0500

    several changes

--- front-ends/toy_shop-web/js/app.js
+++ front-ends/toy_shop-web/js/app.js
@@ -24,7 +24,7 @@ var text_locations = {
 	mailto: stext,
 	contactphone: stext,
 	sitetittle:stext,
-	serevicetab: stext,
+	servicetab: stext,
 	heading1: stext,
 	heading11:stext,
 	heading1text:stext,
